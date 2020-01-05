e51ddf264684bfe62fdafd2434eaa9dbd59ea6aa
a-turko <andrzej.turko@gmail.com>
2020-01-05 14:38:48 +0100

    Implemented adding edges

--- main.cpp
+++ main.cpp
@@ -5,13 +5,97 @@ using namespace std;
 const int L = 4;
 const int N = 1<<L; //maximal number of vertices
 const int M = 100; //maximal number of edge insertions
-struct Holm {
+class Holm {
 	pair <int, int> Edge[M];
-	bool Valid[M];
-	int Lvl[M];
+	bool Tree[M];
+	int Lvl[M], n, log, id;
 	Forest FT[L], FNT[L];
 	
-	int n, log, id;
+	bool getTreeEdge(int f, int v, int &eId) {
+		do {
+			eId = FNT[f].get(v).value_or(-1);
+			
+			if (eId==-1) return false;
+			
+		}while (Lvl[eId]!=f);
+		
+		return true;
+	}
+	
+	bool getNonTreeEdge(int f, int v, int &eId) {	
+		do {
+			eId = FNT[f].get(v).value_or(-1);
+			
+			if (eId==-1) return false;
+		}while (Lvl[eId]!=f);
+		
+		return true;
+	}
+	
+	void addTreeEdge(int f, int eId) {
+		
+		FT[f].addEdge(Edge[eId].first, Edge[eId].second);
+		FNT[f].addEdge(Edge[eId].first, Edge[eId].second);
+	}
+	
+	void storeTreeEdge(int f, int eId) {
+		FT[f].store(Edge[eId].first, eId);
+		FT[f].store(Edge[eId].second, eId);
+		Lvl[eId] = f;
+		
+		addTreeEdge(f, eId);
+	}
+	
+	void storeNonTreeEdge(int f, int eId) {
+		FNT[f].store(Edge[eId].first, eId);
+		FNT[f].store(Edge[eId].second, eId);
+		assert(Lvl[eId]==-1);
+		Lvl[eId] = f;
+	}
+	
+	void removeNonTreeEdge (int f, int eId) {
+		Lvl[eId] = -1;
+	}
+	
+	
+	
+	public:
+	
+	bool query (int a, int b) {
+		return FT[0].sameTree(a,b);
+	}
+	
+	bool addEdge (int a, int b) {
+		Lvl[id] = -1;
+		Tree[id] = false;
+		Edge[id] = {a,b};
+		
+		bool ret = !FT[0].sameTree(a,b);
+		
+		if (ret) {
+			storeTreeEdge(0, id);
+			Tree[id] = true;
+		}
+		else {
+			storeNonTreeEdge(0, id);
+		}
+		
+		id++;
+		return ret;
+	}
+	
+	void removeEdge (int eId) {
+		
+		if (!Tree[eId]) {
+			assert(Lvl[eId]>=0);
+			removeNonTreeEdge(Lvl[eId], eId);
+		}
+		else {	/*it was a tree edge */
+		
+			
+		}	
+	}
+	
 	void init (int _n) {
 		n = _n;
 		log = 0;
@@ -19,47 +103,9 @@ struct Holm {
 		while ((1<<log) < n) log++;
 		
 		for (int l=0; l<=log; l++) {
-			F[l] = Forest(n);
+			FT[l] = Forest(n);
+			FNT[l] = Forest(n);
 		}
-	}
-	
-	bool query (int a, int b) {
-		return FT[0].sameTree(a,b);
-	}
-	
-	void removeNonTreeEdge (int f, int eId) {
-		
-	}
-	
-	void removeTreeEdge (int f, int eId) {
-		
-	}
-	
-	
-	bool getTreeEdge(int f, int v, int &eId) {
-		
-	}
-	
-	bool getNonTreeEdge(int f, int v, int &eId) {
-		
-	}
-	
-	void addTreeEdge(int f, int eId) {
-		
-	}
-	
-	void addNonTreeEdge(int f, int eId) {
-		
-		
-	}
-	
-	bool addEdge (int a, int b) {
-		
-	}
-	
-	void removeEdge (int eId) {
-		Valid[eId] = false;
-		
 	}
 };	
 
